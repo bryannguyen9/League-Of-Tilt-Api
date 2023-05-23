79684c74a843c693fad215103273ec07318b22df
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-05-22 18:15:37 -0700

    finished championController.js

--- controllers/championController.js
+++ controllers/championController.js
@@ -1,38 +1,121 @@
 const { Champion, Thought } = require('./models');
 
 const championController = {
-  getAllChampions(req, res) {
-    // Implement logic to get all Champions from the database
-    // and return the data as JSON response
+  getAllChampions: async (req, res) => {
+    // get all Champions from the database
+    try {
+      const champions = await Champion.find({})
+      .populate('thoughts')
+      .populate('friends');
+      res.json(champions);
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500).json(err);
+    }
   },
 
-  getChampionById(req, res) {
-    // Implement logic to get a single Champion by ID from the database
-    // and return the data as JSON response
+  getChampionById: async (req, res) => {
+    // get a single Champion by ID from the database
+    try {
+      const { id } = req.params;
+      const champion = await Champion.findOne({ _id: id })
+      .populate('thoughts')
+      .populate('friends');
+      if(!champion) {
+        res.status(404).json({ message: 'No Champion found with this id!' });
+        return;
+      }
+      res.json(champion);
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500).json(err);
+    }
   },
 
-  createChampion(req, res) {
-    // Implement logic to create a new Champion in the database
-    // based on the request body data
+  createChampion: async (req, res) => {
+    try {
+      const champion = await Champion.create(req.body);
+      res.json(champion);
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
   },
 
-  updateChampion(req, res) {
-    // Implement logic to update a Champion by ID in the database
-    // based on the request body data
+  updateChampion: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const updatedChampion = await Champion.findOneAndUpdate(
+        { _id: id },
+        req.body,
+        { new: true, runValidators: true }
+      );
+      if (!updatedChampion) {
+        res.status(404).json({ message: 'No champion found with this id' });
+        return;
+      }
+      res.json(champion);
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
   },
 
-  deleteChampion(req, res) {
-    // Implement logic to delete a Champion by ID from the database
+  deleteChampion: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deletedChampion = await Champion.findOneAndDelete({ _id: id });
+      if (!deletedChampion) {
+        res.status(404).json({ message: 'No champion found with this id' });
+        return;
+      }
+      await Champion.updateMany(
+        { _id: { $in: champion.friends } },
+        { $pull: { friends: id } }
+      );
+      res.json({ message: 'Champion deleted successfully' });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
   },
 
-  addFriend(req, res) {
-    // Implement logic to add a friend to a Champion's friend list
-    // based on the request parameters (ChampionId, friendId)
+  addFriend: async (req, res) => {
+    try {
+      const { championId, friendId } = req.params;
+      const champion = await Champion.findOneAndUpdate(
+        { _id: championId },
+        { $addToSet: { friends: friendId } },
+        { new: true }
+      );
+      if (!champion) {
+        res.status(404).json({ message: 'No champion found with this id' });
+        return;
+      }
+      res.json(champion);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   },
 
-  removeFriend(req, res) {
-    // Implement logic to remove a friend from a Champion's friend list
-    // based on the request parameters (ChampionId, friendId)
+  removeFriend: async (req, res) => {
+    try {
+      const { championId, friendId } = req.params;
+      const champion = await Champion.findOneAndUpdate(
+        { _id: championId },
+        { $pull: { friends: friendId } },
+        { new: true }
+      );
+      if (!champion) {
+        res.status(404).json({ message: 'No champion found with this id' });
+        return;
+      }
+      res.json(champion);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   }
 };
 
